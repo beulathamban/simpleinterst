7f0da43d10f193fffc541549bab2c3e22d81f61b
beulathamban <35990861+beulathamban@users.noreply.github.com>
2021-03-05 12:09:39 +1300

    Create expressions.cpp

---
+++ expressions.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    
+    //input and output variables must be declarated and or initialized before using 
+    
+    int a,b,c,d,f,x,y;
+    
+   float z;
+    
+    
+    //input prompting and receiving
+    
+   // cout << " \n Enter the value for a,b,c,d,x, y : \t ";
+   // cin >> a >> b >> c >> d >> x >> y;
+    
+    //calculation and process of expressions - BODMAS
+    
+  // f = a * x + b;
+    
+   // z = x * x + y * y;
+    
+   // z = (float)a * x +  (float)(b - c) * (c -d) /y;
+    z = rand(5);
+    //output
+    
+   //cout << " \n The value of ax+b is \t" << c << endl;
+   cout << "\n The value of z is \t" << z << endl;
+    
+    
+    // Simple interest
+    
+ 
+    /*float p,r,si ;
+    int n;
+   cout << " \n Enter the principle amount NZ$\t";
+   cin >> p;
+    cout << " \n Enter the number of months\t";
+    cin >> n;
+    cout << " \n Enter the rate of interest\t";
+    cin >> r;
+    
+    
+    si = (p * n * r) / 100 ;
+    cout << "\n Simple interest = NZ$ \t" << si <<endl;*/
+    
+    
+    
+    
+    
+    
+    return 0;
+}
